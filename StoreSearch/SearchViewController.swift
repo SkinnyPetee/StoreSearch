3da6e1e27ba0a773167d14f1234802c64e908ccd
sriram <sriram@srirams-mac.local>
2018-05-18 04:21:38 -0700

    finished sync networking

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -16,16 +16,19 @@ class SearchViewController: UIViewController,UISearchBarDelegate, UITableViewDel
         if !(searchBar.text?.trimmingCharacters(in: .whitespaces).isEmpty)!{
             searchBar.resignFirstResponder()
             searchResults = []
-            if searchBar.text!  != "justin" {
-            for i in 0...2 {
-                    let searchResult = SearchResult()
-                    searchResult.name = String(format: "Fake Result %d for", i)
-                    searchResult.artistName = searchBar.text!
-                    searchResults.append(searchResult)
-                }
-            }
             hasSearched = true
-            tableView.reloadData()
+            let url = iTunesURL(searchText: searchBar.text!)
+            print("URL: '\(url)'")
+            if let jsonString = performStoreRequest(with: url) {
+                if let jsonDictionary = parse(json: jsonString) {
+                    print("Dictionary \(jsonDictionary)")
+                    searchResults = parse(dictionary: jsonDictionary)
+                    tableView.reloadData()
+                    return
+                }
+                
+            }
+            showNetworkError()
         }
         
         
@@ -33,6 +36,18 @@ class SearchViewController: UIViewController,UISearchBarDelegate, UITableViewDel
     
     func position(for bar: UIBarPositioning) -> UIBarPosition {
         return .topAttached
+    }
+    
+    
+    func showNetworkError() {
+        let alert = UIAlertController(
+            title: "Whoops...",
+            message:
+            "There was an error reading from the iTunes Store. Please try again.",
+            preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
     var searchResults: [SearchResult] = []
@@ -83,7 +98,84 @@ cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         }
     }
     
+    func performStoreRequest(with url: URL) -> String? {
+        do {
+            return try String(contentsOf: url, encoding: .utf8)
+        } catch {
+            print("Download Error: \(error)")
+            return nil
+        }
+    }
     
+    func iTunesURL(searchText: String) -> URL {
+        let escapedSearchText = searchText.addingPercentEncoding(
+            withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+        let urlString = String(format:
+            "https://itunes.apple.com/search?term=%@", escapedSearchText)
+        let url = URL(string: urlString)
+        return url!
+    }
+    
+    func parse(dictionary: [String: Any]) -> [SearchResult] {
+        guard let array = dictionary["results"] as? [Any] else {
+            print("Expected 'results' array")
+            return []
+        }
+        var searchResults: [SearchResult] = []
+        // 2
+        for resultDict in array {
+            // 3
+            if let resultDict = resultDict as? [String: Any] {
+                // 4
+                var searchResult: SearchResult?
+                if let wrapperType = resultDict["wrapperType"] as? String {
+                    switch wrapperType {
+                    case "track":
+                        searchResult = parse(track: resultDict)
+                    default:
+                        break
+                    }
+                }
+                if let result = searchResult {
+                    searchResults.append(result)
+                }
+            }
+            
+        }
+        return searchResults
+        
+    }
+    
+    func parse(json: String) -> [String: Any]? {
+        guard let data = json.data(using: .utf8, allowLossyConversion: false)
+            else { return nil }
+        do {
+            return try JSONSerialization.jsonObject(
+                with: data, options: []) as? [String: Any]
+        } catch {
+            print("JSON Error: \(error)")
+            return nil
+        }
+    }
+    
+    
+    func parse(track dictionary: [String: Any]) -> SearchResult {
+        let searchResult = SearchResult()
+        searchResult.name = dictionary["trackName"] as! String
+        searchResult.artistName = dictionary["artistName"] as! String
+        searchResult.artworkSmallURL = dictionary["artworkUrl60"] as! String
+        searchResult.artworkLargeURL = dictionary["artworkUrl100"] as! String
+        searchResult.storeURL = dictionary["trackViewUrl"] as! String
+        searchResult.kind = dictionary["kind"] as! String
+        searchResult.currency = dictionary["currency"] as! String
+        if let price = dictionary["trackPrice"] as? Double {
+            searchResult.price = price
+        }
+        if let genre = dictionary["primaryGenreName"] as? String {
+            searchResult.genre = genre
+        }
+        return searchResult
+    }
     
     
     struct TableViewCellIdentifiers {
